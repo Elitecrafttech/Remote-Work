39f77f827607800e017c7ee46d6a4f66b7f0e37e
Elitecrafttech <Elitecrafttech@gmail.com>
2024-06-29 03:33:07 -1100

    menu commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function menu() {
+    const play = document.querySelector(".play")
+    play.style.display='flex'
+}
+function closemenu() {
+    const close = document.querySelector(".play")
+    close.style.display = 'none'
+    
+}
